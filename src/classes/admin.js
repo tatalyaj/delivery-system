aa798e971e8cfde7da5f4a4d63d7759867590bd1
Talya <tatalya97@gmail.com>
2023-03-21 19:34:52 +0200

    commit - update for all properties+ properties

--- src/classes/admin.js
+++ src/classes/admin.js
@@ -1,12 +1,68 @@
 // ADDRESSSES
 const addresses = [
-  { id: 0, name: "Jerusalem", frequency: 0 },
-  { id: 1, name: "Haifa", frequency: 0 },
-  { id: 2, name: "Tel-Aviv", frequency: 0 },
-  { id: 3, name: "Eilat", frequency: 0 },
-  { id: 4, name: "Ra'anana", frequency: 0 },
-  { id: 5, name: "Zichron", frequency: 0 },
-  { id: 6, name: "Tiberia", frequency: 0 },
+  {
+    id: 0,
+    city: "Jerusalem",
+    address: "King george 22",
+    deliveryType: "medicine",
+    frequency: 0,
+    recipientName: "John",
+    recipientPhone: "0528736440",
+  },
+  {
+    id: 1,
+    city: "Haifa",
+    address: "Bar gyora 10",
+    deliveryType: "medicine",
+    frequency: 0,
+    recipientName: "James",
+    recipientPhone: "0503827110",
+  },
+  {
+    id: 2,
+    city: "Tel-Aviv",
+    address: "Shenkin 20",
+    deliveryType: "medicine",
+    frequency: 0,
+    recipientName: "Sarah",
+    recipientPhone: "0507231009",
+  },
+  {
+    id: 3,
+    city: "Eilat",
+    address: "Hanachal 12",
+    deliveryType: "medicine",
+    frequency: 0,
+    recipientName: "Josh",
+    recipientPhone: "026745899",
+  },
+  {
+    id: 4,
+    city: "Ra'anana",
+    address: "Haneviim 3",
+    deliveryType: "food",
+    frequency: 0,
+    recipientName: "Julian",
+    recipientPhone: "0507837664",
+  },
+  {
+    id: 5,
+    city: "Zichron",
+    address: "Neve 15",
+    deliveryType: "food",
+    frequency: 0,
+    recipientName: "Natasha",
+    recipientPhone: "0523478660",
+  },
+  {
+    id: 6,
+    city: "Tiberia",
+    address: "Kineret 6",
+    deliveryType: "food",
+    frequency: 0,
+    recipientName: "Ron",
+    recipientPhone: "0504323119",
+  },
 ];
 export default class Admin {
   //constructor() {}
@@ -15,18 +71,42 @@ export default class Admin {
     return addresses;
   }
 
-  editAddress(id, newAddress) {
+  editAddress(
+    id,
+    updatedCity,
+    updatedAddress,
+    updatedDeliveryType,
+    updatedFrequency,
+    updatedRecipientName,
+    updatedRecipientPhone
+  ) {
     //console.log(id);
     //console.log(newAddress);
     const item = addresses.find((i) => i?.id === id);
-    addresses[addresses.indexOf(item)].name = newAddress;
+    addresses[addresses.indexOf(item)].city = updatedCity;
+    addresses[addresses.indexOf(item)].address = updatedAddress;
+    addresses[addresses.indexOf(item)].deliveryType = updatedDeliveryType;
+    addresses[addresses.indexOf(item)].frequency = updatedFrequency;
+    addresses[addresses.indexOf(item)].recipientName = updatedRecipientName;
+    addresses[addresses.indexOf(item)].recipientPhone = updatedRecipientPhone;
   }
 
-  addAddress(name, frequency) {
+  addAddress(
+    newCity,
+    newAddress,
+    newDeliveryType,
+    newFrequency,
+    newRecipientName,
+    newRecipientPhone
+  ) {
     addresses.push({
       id: addresses.length,
-      name: name,
-      frequency: frequency,
+      city: newCity,
+      address: newAddress,
+      deliveryType: newDeliveryType,
+      frequency: newFrequency,
+      recipientName: newRecipientName,
+      recipientPhone: newRecipientPhone,
     });
   }
 
--- src/pages/admin2.js
+++ src/pages/admin2.js
@@ -12,7 +12,18 @@ export default class AdminPage extends React.Component {
       addresses: adminService.getAddresses(),
       addressForEdit: null,
       showDialog: false,
+      updatedCity: null,
       updatedAddress: null,
+      updatedDeliveryType: null,
+      updatedFrequency: null,
+      updatedRecipientName: null,
+      updatedRecipientPhone: null,
+      newCity: null,
+      newAddress: null,
+      newDeliveryType: null,
+      newFrequency: null,
+      newRecipientName: null,
+      newRecipientPhone: null,
     };
 
     // this.getAddresses();
@@ -33,43 +44,137 @@ export default class AdminPage extends React.Component {
     this.setState({
       showDialog: false,
       addressForEdit: null,
+      updatedCity: null,
       updatedAddress: null,
+      updatedDeliveryType: null,
+      updatedFrequency: null,
+      updatedRecipientName: null,
+      updatedRecipientPhone: null,
+      newCity: null,
+      newAddress: null,
+      newDeliveryType: null,
+      newFrequency: null,
+      newRecipientName: null,
+      newRecipientPhone: null,
     });
   }
+
   // For the "UPDATE" scenario
-  handleChange(addressName) {
-    //console.log(addressName);
-    this.setState({ updatedAddress: addressName });
-  }
-  handleEditAddress(id, addressName) {
+  handleChange = (e) => {
+    // const value = e.target.value;
+    //if(e.className == "edit.button")
+    const { name, value } = e.target;
+    console.log(name, value);
+    //console.log([name]);
+    // this.setState({ ...this.state, [e.target?.name]: value });
+    this.setState({ [name]: value });
+    //console.log(this.state.updatedAddress);
+  };
+  //   handleChange = (e) => {
+  //     const updateNameKey = "updated";
+  //     const newNameKey = "new";
+  //     console.log(e);
+  //     console.log(e.className);
+  //     if (e.className === "edit.button") {
+  //       const { name, value } = e.target;
+  //       const updatedName = updateNameKey + name;
+  //       console.log(updatedName);
+  //       this.setState({ [updatedName]: value });
+  //     } else if (e.className === "add.button") {
+  //       const { name, value } = e.target;
+  //       const newName = newNameKey + name;
+  //       console.log(newName);
+  //       this.setState({ [newName]: value });
+  //     }
+  //   };
+  handleEditAddress(
+    id,
+    city,
+    address,
+    deliveryType,
+    frequency,
+    recipientName,
+    recipientPhone
+  ) {
     //his.setState({ updatedAddress: addressName });
     //console.log(addressName);
-    adminService.editAddress(id, addressName);
+    console.log(
+      id,
+      city,
+      address,
+      deliveryType,
+      frequency,
+      recipientName,
+      recipientPhone
+    );
+
+    adminService.editAddress(
+      id,
+      city,
+      address,
+      deliveryType,
+      frequency,
+      recipientName,
+      recipientPhone
+    );
     this.getAddresses();
   }
 
   handleShowEditDialog(address) {
     this.setState({ showDialog: true, addressForEdit: address });
   }
+  // The "ADD" scenario
+  handleShowAddDialog(e) {
+    this.setState({ showDialog: true });
+    console.log(e);
+  }
+
+  handleAddAddress(
+    city,
+    address,
+    deliveryType,
+    frequency,
+    recipientName,
+    recipientPhone
+  ) {
+    adminService.addAddress(
+      city,
+      address,
+      deliveryType,
+      frequency,
+      recipientName,
+      recipientPhone
+    );
+    this.getAddresses();
+  }
 
   render() {
     return (
       <div>
-        <table className="table">
+        <button
+          className="add-button "
+          onClick={(e) => this.handleShowAddDialog(e)}
+        >
+          Add
+        </button>
+        <table className="addresses-table">
           <tbody>
             <thead>
               <td>Name</td>
             </thead>
             {this.state.addresses?.map((item) => (
               <tr key={item?.id}>
-                <td>{item?.name}</td>
+                <td>{item?.city}</td>
                 <td>
                   <button onClick={(e) => this.handleDelete(item.id, e)}>
                     Delete
                   </button>
                 </td>
                 <td>
-                  <button onClick={(e) => this.handleShowEditDialog(item, e)}>
+                  <button
+                    className="edit-buttons"
+                    onClick={(e) => this.handleShowEditDialog(item, e)}
+                  >
                     Edit
                   </button>
                 </td>
@@ -88,10 +193,57 @@ export default class AdminPage extends React.Component {
           <Modal.Body>
             {/* <p>{this.state.addressForEdit?.name}</p> */}
             <label>
-              Updated address name:
+              City:
               <input
                 type="text"
-                onChange={(e) => this.handleChange(e.target.value)}
+                name="updatedCity"
+                value={this.state.updatedCity}
+                onChange={this.handleChange}
+              ></input>
+            </label>
+            <label>
+              Address:
+              <input
+                type="text"
+                name="updatedAddress"
+                value={this.state.updatedAddress}
+                onChange={this.handleChange}
+              ></input>
+            </label>
+            <label>
+              Delivery type:
+              <input
+                type="text"
+                name="updatedDeliveryType"
+                value={this.state.updatedDeliveryType}
+                onChange={this.handleChange}
+              ></input>
+            </label>
+            <label>
+              Frequency:
+              <input
+                type="text"
+                name="updatedFrequency"
+                value={this.state.updatedFrequency}
+                onChange={this.handleChange}
+              ></input>
+            </label>
+            <label>
+              Recipient name:
+              <input
+                type="text"
+                name="updatedRecipientName"
+                value={this.state.updatedRecipientName}
+                onChange={this.handleChange}
+              ></input>
+            </label>
+            <label>
+              Recipient Phone number:
+              <input
+                type="text"
+                name="updatedRecipientPhone"
+                value={this.state.updatedRecipientPhone}
+                onChange={this.handleChange}
               ></input>
             </label>
           </Modal.Body>
@@ -104,11 +256,32 @@ export default class AdminPage extends React.Component {
               onClick={() =>
                 this.handleEditAddress(
                   this.state.addressForEdit?.id,
-                  this.state.updatedAddress
+                  this.state.updatedCity,
+                  this.state.updatedAddress,
+                  this.state.updatedDeliveryType,
+                  this.state.updatedFrequency,
+                  this.state.updatedRecipientName,
+                  this.state.updatedRecipientPhone
                 )
               }
             >
               Save Changes
+            </Button>
+            <Button
+              variant="primary"
+              //   style={{ display: "none" }}
+              onClick={() =>
+                this.handleAddAddress(
+                  this.state.newCity,
+                  this.state.newAddress,
+                  this.state.newDeliveryType,
+                  this.state.newFrequency,
+                  this.state.newRecipientName,
+                  this.state.newRecipientPhone
+                )
+              }
+            >
+              Add Address
             </Button>
           </Modal.Footer>
         </Modal>
